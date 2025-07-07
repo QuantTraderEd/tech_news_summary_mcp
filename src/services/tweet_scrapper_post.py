dc3ceca9d634258c2194e853e772ee47206c2bad
QuantTraderEd <hyojkim79@gmail.com>
2025-07-07 05:10:28 +0000

    refactor: 개별 post.json gcs upload 중복 기능 제거

--- src/services/tweet_scrapper_post.py
+++ src/services/tweet_scrapper_post.py
@@ -367,12 +367,7 @@ def main(base_ymd: str):
 
         # --- 지정된 모든 사용자에 대해 스크래핑 실행 ---
         for user in TARGET_USERNAMES:
-            tweet_scraper.scrape_user_post(user)
-            output_filename = f"{pjt_home_path}/data/{user}_posts.json"
-            if os.path.exists(output_filename): 
-                gcs_upload_json.upload_local_file_to_gcs(local_file_path=output_filename,
-                                                        date_str=base_ymd
-                                                        )
+            tweet_scraper.scrape_user_post(user)            
 
         # 모든 작업이 끝나면 브라우저 종료
         if tweet_scraper.driver: tweet_scraper.driver.quit()
